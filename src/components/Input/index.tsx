dd9c0460cd66c988a0e0d4e7f2547a72f14475a5
Fábio Santos <fabio.santos@fundacaoabc.org>
2020-09-20 14:14:06 -0300

    feat: instalação do unform e integrado ao SignUp
    Utilizado o conceito de registerField, fieldName, defaulValue, useRef para geração do input e
    captação dos valores digitados quando foram enviados pelo form.

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,11 @@
-import React, { ComponentType, InputHTMLAttributes } from 'react';
+import React, {
+  ComponentType,
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+} from 'react';
 import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
 import { Container } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -7,11 +13,33 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   icon?: ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => {
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const {
+    clearError,
+    defaultValue,
+    error,
+    fieldName,
+    registerField,
+  } = useField(name);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
   return (
     <Container>
       {Icon && <Icon size={16} />}
-      <input {...rest} />
+      <input
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+        onFocus={clearError}
+      />
     </Container>
   );
 };
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 
 import { FiArrowLeft, FiLock, FiMail, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 import { Container, Background, Content } from './styles';
 import Logo from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 const SignUp: React.FC = () => {
+  const handleOnSubmit = (data: object) => {
+    console.log(data);
+  };
+
   return (
     <Container>
       <Background />
       <Content>
         <img src={Logo} alt="GoBarber" />
-        <form>
+        <Form
+          // initialData={{ name: 'Fábio dos Santos' }}
+          onSubmit={handleOnSubmit}
+        >
           <h1>Faça seu cadastro</h1>
-          <Input name="nome" type="text" icon={FiUser} placeholder="Nome" />
+          <Input name="name" type="text" icon={FiUser} placeholder="Nome" />
           <Input name="email" type="email" icon={FiMail} placeholder="E-mail" />
           <Input
             name="password"
@@ -22,8 +30,8 @@ const SignUp: React.FC = () => {
             type="password"
             placeholder="Senha"
           />
-          <Button>Cadastrar</Button>
-        </form>
+          <Button type="submit">Cadastrar</Button>
+        </Form>
         <a href="create">
           <FiArrowLeft />
           Voltar para o login
