207165ea5c4779c21e93c74659a2028dfe79c2c8
Fábio Santos <fabio.santos@fundacaoabc.org>
2020-12-06 16:10:23 -0300

    feat: correção de nome do localstorage

--- src/hooks/Auth.tsx
+++ src/hooks/Auth.tsx
@@ -58,7 +58,7 @@ export const AuthProvider: React.FC = ({ children }) => {
 
   const updateUser = useCallback(
     (user: User) => {
-      localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+      localStorage.setItem('@GoBarber-user', JSON.stringify(user));
 
       setData({
         token: data.token,
